ebaf4b5996b91071b4d308c01385a33fe680914f
Keith Salo <keithsalo@gmail.com>
2023-01-15 12:07:12 -0800

    fortune game -1/15

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
-from datetime import datetime 
+from datetime import datetime
+
+formData = {}
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 with app.app_context():
     db = SQLAlchemy(app)
+
 
 class Todo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -14,20 +17,28 @@ class Todo(db.Model):
 
     def __repr__(self):
         return '<Thing %r>' % self.id
-    
+
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/about/')
+
+@app.route('/about')
 def about():
     return render_template('about.html')
 
-@app.route('/contact/')
-def contact():
-    return render_template('contact.html')
 
+@app.route('/game', methods=['GET', 'POST'])
+def game():
+    if request.method == 'POST':
+        name = request.form.get("user")
+        colors = request.form.get("color")
+        digit = request.form.get("number")
+
+        return (f"Hi {name.capitalize()}, you selected the color  {colors.capitalize()} and the number  {digit}.  You are lucky! Play the Lotto!")
+
+    return render_template('game.html')
 
 
 if __name__ == "__main__":
